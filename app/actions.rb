7172ee50734b886ce92b450d3809a10a5045bdec
scottdomes <scottdomes@gmail.com>
2016-03-24 16:49:38 -0700

    Added basic user page styling

--- app/actions.rb
+++ app/actions.rb
@@ -5,7 +5,7 @@ enable :sessions
 before do
   User.destroy_all
   Goal.destroy_all
-  @scott = User.create(name: "Scott D-dog")
+  @scott = User.create(name: "Scott D-dog", email: "scottdomes@gmail.com")
   5.times do
     @scott.goals << Goal.create(title: "Scott wants to finish this project!", stake_qty: 2, stake_item: "beer", deadline: Date.tomorrow)
   end
@@ -16,4 +16,9 @@ end
 # Homepage (Root path)
 get '/' do
   erb :goals
+end
+
+get '/users/1' do
+  @user = @scott
+  erb :'users/show'
 end
