1e3e1e791fd4fa074ba9e2a8801607a44864d113
scottdomes <scottdomes@gmail.com>
2016-03-25 15:17:08 -0700

    Added redirect back after adding new goal

--- app/user-actions.rb
+++ app/user-actions.rb
@@ -1,6 +1,11 @@
 helpers do
   def stringify_bet(user, bet)
-    string = user.first_name + " bet " + User.find(bet.goal.user_id).first_name + " " + bet.goal.stake_qty.to_s + " " + bet.goal.stake_item + " that " + User.find(bet.goal.user_id).first_name + " would " + bet.goal.title + " by " + bet.goal.deadline.strftime("%m:%M %p on %A, %B %e") + "!"
+    string = user.first_name + " bet "
+    string += User.find(bet.goal.user_id).first_name + " "
+    string += bet.goal.stake_qty.to_s + " " + bet.goal.stake_item
+    string += " that " + User.find(bet.goal.user_id).first_name 
+    string += " would " + bet.goal.title 
+    string += " by " + bet.goal.deadline.strftime("%m:%M %p on %A, %B %e") + "!"
     string.gsub(/[^A-Za-z0-9\s]/i, '')
   end
 
