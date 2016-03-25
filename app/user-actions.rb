7517e2004bfa03f08793c7c01ff3c7c43f90d805
scottdomes <scottdomes@gmail.com>
2016-03-25 13:47:43 -0700

    Added bet status helper method

--- app/user-actions.rb
+++ app/user-actions.rb
@@ -1,3 +1,21 @@
+helpers do
+  def stringify_bet(user, bet)
+    string = user.first_name + " bet " + User.find(bet.goal.user_id).first_name + " " + bet.goal.stake_qty.to_s + " " + bet.goal.stake_item + " that " + User.find(bet.goal.user_id).first_name + " would " + bet.goal.title + " by " + bet.goal.deadline.strftime("%m:%M %p on %A, %B %e") + "!"
+    string.gsub(/[^A-Za-z0-9\s]/i, '')
+  end
+
+  def bet_winner(bet)
+    if bet.goal.success
+      User.find(bet.goal.user_id).first_name + " wins!"
+    elsif bet.goal.fail
+      bet.user.first_name + " wins!"
+    else
+      "Unresolved!"
+    end
+  end
+end
+
+
 post '/goals/complete' do
   @goal = Goal.find(params[:goal_id])
   if params[:success] == "true"
