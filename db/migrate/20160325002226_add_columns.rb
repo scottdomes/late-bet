1ffae31e2de60fffaff8f91182d6381f6ef26f19
Jules Wei <s.jules2000@gmail.com>
2016-03-25 00:40:23 +0000

    created new colomns in user and goals and deleted name

---
+++ db/migrate/20160325002226_add_columns.rb
@@ -0,0 +1,20 @@
+class AddColumns < ActiveRecord::Migration
+  def change
+      
+      remove_column :users, :name
+
+      change_table :goals do |t|
+        t.boolean :fail, default: false
+        t.boolean :success, default: false
+      end  
+
+      change_table :users do |t|
+        t.string :first_name
+        t.string :last_name
+        t.string :username
+      end  
+
+  end
+
+
+end
