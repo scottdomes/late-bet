ee0756bcd2d26cc1aa0d33d2f60ffc19e8284246
scottdomes <scottdomes@gmail.com>
2016-03-24 17:12:16 -0700

    Add new goal now works

--- app/actions.rb
+++ app/actions.rb
@@ -3,12 +3,12 @@ require_relative 'goal-actions'
 enable :sessions
 
 before do
-  User.destroy_all
-  Goal.destroy_all
-  @scott = User.create(name: "Scott D-dog", email: "scottdomes@gmail.com")
-  5.times do
-    @scott.goals << Goal.create(title: "Scott wants to finish this project!", stake_qty: 2, stake_item: "beer", deadline: Date.tomorrow)
-  end
+  # User.destroy_all
+  # Goal.destroy_all
+  @scott = User.find_by(name: "Scott D-dog")
+  # 5.times do
+  #   @scott.goals << Goal.create(title: "Scott wants to finish this project!", stake_qty: 2, stake_item: "beer", deadline: Date.tomorrow)
+  # end
   session[:user] = @scott # TEST
 end
 
--- app/goal-actions.rb
+++ app/goal-actions.rb
@@ -8,12 +8,28 @@ get '/goals' do
   # end
 end
 
-get '/goals/new' do
-  # if session[:user]
-  #   @goal = Goal.new
-    erb :'goals/new'
-  # else
-  #   cookies[:]
-  #   redirect '/goals/'
-  # end
+# get '/goals/new' do
+#   # if session[:user]
+#   #   @goal = Goal.new
+#     erb :'goals/new'
+#   # else
+#   #   cookies[:]
+#   #   redirect '/goals/'
+#   # end
+# end
+
+post '/goals' do
+  @goal = Goal.new(
+    title: params[:title],
+    deadline: params[:deadline],
+    stake_qty: params[:stake_qty],
+    stake_item: params[:stake_item],
+    user_id: session[:user].id
+  )
+  if @goal.save
+    redirect '/'
+  else 
+    @goal_errors = @goal.errors.full_messages
+    erb :'goals'
+  end
 end
