3db9b05096e842b61e77d8370184c9b6b1c70462
scottdomes <scottdomes@gmail.com>
2016-03-26 14:33:33 -0700

    Added goal notifications

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -15,10 +15,32 @@ class Goal < ActiveRecord::Base
   #could also validate with JavaScript
   validate :deadline_date_cannot_be_in_the_past, if: :deadline
 
+  after_update :add_goal_notification
+
   def deadline_date_cannot_be_in_the_past
     if deadline < Date.today
       errors.add(:deadline, "Dates can't be in the past.")
     end
   end    
+
+  def add_goal_notification
+    if success
+      bets.each do |bet|
+        bet.user.notifications.create(
+          other_user_id: user.id, 
+          content: "#{user.first_name} succeeded at their goal to #{title}! You owe #{user.first_name} ${stake_qty} #{stake_item}!"
+        )
+        bet.user.save
+      end
+    elsif fail
+      bets.each do |bet|
+        bet.user.notifications.create(
+          other_user_id: user.id, 
+          content: "#{user.first_name} failed at their goal to #{title}! You won #{stake_qty} #{stake_item} from #{user.first_name}!"
+        )
+        bet.user.save
+      end
+    end
+  end
 
 end
