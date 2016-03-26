5d23ea59fc8752ee7a10a8d8f63111fa9961be39
scottdomes <scottdomes@gmail.com>
2016-03-26 15:49:50 -0700

    Added feature to grey out notifications on reading- decided against it afterwards

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -11,6 +11,10 @@ $(document).ready(function() {
       url: "/users/empty_notifications",
       type: 'POST'
     });
+    // Greys out before reading. May need resurrecting with modification
+    // $.each($('#notifications-menu li'), function () {
+    //   $(this).addClass('read-notification');
+    // })
     $('#notifications-button').removeClass('live');
   });
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
