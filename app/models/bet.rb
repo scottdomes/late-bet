ee73b7833f6ec698d267d60233b21bb0e0eaa48e
scottdomes <scottdomes@gmail.com>
2016-03-26 14:21:57 -0700

    Added basic code for creating notification on bet save

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -5,9 +5,20 @@ class Bet < ActiveRecord::Base
   validate :user_cannot_bet_on_self
   validates_uniqueness_of :user_id, :message => " cannot make same bet twice"
 
+  after_create :add_bet_notification
+
   def user_cannot_bet_on_self
     if goal.user == user
       errors.add(:user, " cannot bet on self!")
     end
   end
+
+  def add_bet_notification
+    goal.user.notifications.create(
+      other_user_id: user.id, 
+      content: "#{user.first_name} bet on your goal to #{goal.title}!"
+    )
+    goal.user.save
+  end
+
 end
