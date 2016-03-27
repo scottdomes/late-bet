79d5ba910a4a298804e00bf28eccceda03311ebc
scottdomes <scottdomes@gmail.com>
2016-03-27 16:12:46 -0700

    Got basic ajax working:

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -17,5 +17,44 @@ $(document).ready(function() {
     // })
     $('#notifications-button').removeClass('live');
   });
+
+  // $('#submit-goal-button').click(function () {
+  //   $("#add-goal-form").ajaxForm({url: '/goals', type: 'post'});
+  //   $("#active-goals").load(location.href);
+  // })
+
+    // $('#add-goal-form').submit(function(e){
+    //       e.preventDefault();
+    //       var form = $(this);
+    //       var post_url = form.attr('action');
+    //       var post_data = form.serialize();
+    //       $.ajax({
+    //           type: 'POST',
+    //           url: post_url, 
+    //           data: post_data,
+    //           success: function(msg) {
+    //               $(form).fadeOut(800, function(){
+    //                   form.html(msg).fadeIn().delay(1000);
+    //               });
+                
+    //           }
+    //       });
+    //   });
+
+  $('#add-goal-form').submit(function(e){
+    e.preventDefault();
+    var form = $(this);
+    var post_url = form.attr('action');
+    $.ajax({
+        url: post_url,
+        type: 'POST',
+        dataType: 'html',
+        data: $(this).serialize(),
+        success: function(data) {
+         $('#active-goals-wrapper').load(location.href + " #active-goals-grid"); 
+      }
+    });
+   return false;
+});
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
