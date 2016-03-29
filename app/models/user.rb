9bb498745e65d446b6b625d4164ac0a82c26b1b3
scottdomes <scottdomes@gmail.com>
2016-03-28 21:23:48 -0700

    Changed mind on winnings, kept it as hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,58 +20,22 @@ class User < ActiveRecord::Base
     bets.each do |bet|
       if bet.goal.fail
         results[:winnings] << bet
-      else
+      elsif bet.goal.success
         results[:debts] << bet
       end
     end
 
     goals.each do |goal|
       if goal.success && goal.bets
-        goals.bets.each do |bet|
+        goal.bets.each do |bet|
           results[:winnings] << bet
         end
       elsif goal.fail && goal.bets
-        goals.bet.each do |bet|
+        goal.bet.each do |bet|
           results[:debts] << bet
         end
       end
     end
+    results
   end
-
-  def winnings
-    winnings = []
-    bets.each do |bet|
-      if bet.goal.fail
-        winnings << bet
-      end
-    end
-
-    goals.each do |goal|
-      if goal.success && goal.bets
-        goal.bets.each do |bet|
-          winnings << bet
-        end
-      end
-    end
-    winnings
-  end
-
-  def debts
-    debts = []
-    bets.each do |bet|
-      if bet.goal.success == true
-        debts << bet
-      end
-    end
-
-    goals.each do |goal|
-      if goal.fail && goal.bets
-        goal.bets.each do |bet|
-          debts << bet
-        end
-      end
-    end
-    debts
-  end
-
 end
