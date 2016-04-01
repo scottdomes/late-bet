501fb3e7c33e6f3b83f77fcee49791eb89dd8c02
scottdomes <scottdomes@gmail.com>
2016-03-31 18:05:11 -0700

    Reverted to db:host

--- config/database.rb
+++ config/database.rb
@@ -20,7 +20,7 @@ end
   db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
   ActiveRecord::Base.establish_connection(
     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => localhost,
+    :host     => db.host,
     :username => db.user,
     :password => db.password,
     :database => db.path[1..-1],
