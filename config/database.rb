4146452259fc4a8185334ec8468acf9cb9935a7e
scottdomes <scottdomes@gmail.com>
2016-03-31 18:04:15 -0700

    Changed host to localhost

--- config/database.rb
+++ config/database.rb
@@ -20,7 +20,7 @@ end
   db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
   ActiveRecord::Base.establish_connection(
     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
+    :host     => localhost,
     :username => db.user,
     :password => db.password,
     :database => db.path[1..-1],
